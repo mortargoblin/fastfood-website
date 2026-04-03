17e6df7c15c386a889c7f1de4d5c4df468cbe0e6
mortargoblin <mortargoblin@github.com>
2026-04-03 16:48:00 +0300

    buttons

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 const nav = document.querySelector("nav");
+const menuBtn = document.querySelector("#menu");
+const restaurantsBtn = document.querySelector("#restaurants");
+const loginBtn = document.querySelector("#log");
+const infoBtn = document.querySelector("#info");
 
 
 
