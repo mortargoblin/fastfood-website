e20448e0989764a4df3cb6f7e19d682798b29cfb
mortargoblin <mortargoblin@github.com>
2026-04-03 19:08:26 +0300

    swag

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const nav = document.querySelector('nav');
+const homeBtn = document.querySelector('#home');
 const menuBtn = document.querySelector('#menu');
 const restaurantsBtn = document.querySelector('#restaurants');
 const loginBtn = document.querySelector('#log');
@@ -7,25 +8,44 @@ const infoBtn = document.querySelector('#info');
 
 const page = document.querySelector('#page');
 
-function loadPageContent(content, target=page) {
+function loadPageContent(content, target=page, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', content, true);
-  xhr.onreadystatechange = (e) => {
+  xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
       target.innerHTML = xhr.responseText;
-    } else {
-      console.log('wtf', xhr.readyState, this.status)
+
+      if (callback) callback();
     }
-    console.log(e);
   };
   xhr.send();
 }
 
+function updateMenuGrid() {
+  console.log('updateMenuGrid() called')
+
+  const grid = document.querySelector('#menu-grid')
+  grid.innerHTML = '';
+  for (let i = 0; i < 10; i++) {
+    grid.insertAdjacentHTML('beforeend', 
+      `<div class="bg-[#679289] p-4 rounded-xl">
+        <img src="img/bigmac.avif">
+        item
+      </div>`
+    );
+  }
+}
+homeBtn.addEventListener('click', () => {
+  loadPageContent('home.html', page, updateMenuGrid);
+});
+
 menuBtn.addEventListener('click', () => {
-  loadPageContent('menu.html');
+  loadPageContent('menu.html', page, updateMenuGrid);
 });
 
 infoBtn.addEventListener('click', () => {
-
   page.innerHTML = '<h1>kaskdakskdka</h1>'
 });
+
+
+loadPageContent('home.html', page, updateMenuGrid);
