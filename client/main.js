f049516af8af6e4b32372973d470cb1e6f7dbd89
Morso <69962221+Morso33@users.noreply.github.com>
2026-05-06 17:38:19 +0300

    cart responsiveness

--- client/main.js
+++ client/main.js
@@ -69,6 +69,18 @@ async function load()
       console.log(btn.dataset);
       console.log(item);
 
+      Toastify({
+  text: "Added " + item.name + " to cart",
+  duration: 3000,
+  close: true,
+  gravity: "bottom", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  style: {
+    background: "linear-gradient(to right, #00b09b, #96c93d)",
+  },
+  onClick: function(){} // Callback after click
+}).showToast();
+
       addToCart(item);
       renderCart();
     })
